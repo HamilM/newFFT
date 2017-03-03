586d31d323857d38f39ddbbe1cdda687598ed14b
HamilM <hamilis@cs.technion.ac.il>
2017-03-03 02:53:24 +0200

    First commit

---
+++ newFFT/F2XE.cpp
@@ -0,0 +1,10 @@
+/*
+ * F2XE.cpp
+ *
+ *  Created on: Mar 1, 2017
+ *      Author: matt
+ */
+
+#include "F2XE.h"
+
+__m128i clmul_mask = {-1, 0};
---
+++ newFFT/main.cu
@@ -0,0 +1,15 @@
+#ifndef SIZE
+#define SIZE 32
+#endif
+
+#ifndef REPETITIONS
+#define REPETITIONS 5
+#endif
+
+#include <iostream>
+#include "ChunkTesting.cuh"
+
+int main(int argc, char* argv[])
+{
+
+}
